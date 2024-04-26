f4fde8913a85cd14645a96e5030f40ba855eed7c
Ecdysiasttt <99504513+Ecdysiasttt@users.noreply.github.com>
2024-04-26 14:59:10 +0100

    paginated fmodels on user profiles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find_by_id(params[:id])
-    @fmodels = Fmodel.where(created_by: @user.id)
+    @fmodels = Fmodel.where(created_by: @user.id).order(:created_at).page params[:page]
 
     @hasReturn = true
 
